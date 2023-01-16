fe5aa6d23feed9140c7b691e1a15473083d1e674
MamadTvl <mtavallai80@gmail.com>
2023-01-16 07:11:05 +0330

    clean up

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,23 +1,7 @@
-import { Controller, Get, Param, Query, Render, Res } from '@nestjs/common';
-import { randomUUID } from 'crypto';
-import { Response } from 'express';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('room')
 export class AppController {
     constructor(private readonly appService: AppService) {}
-
-    @Get()
-    // @Render('index')
-    index(@Res() res: Response) {
-        res.redirect('/room/' + randomUUID());
-    }
-
-    @Get(':id')
-    @Render('index')
-    getHello(@Param('id') id: string) {
-        console.log('dada');
-
-        return { roomId: id };
-    }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import logger from 'morgan';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -29,8 +28,6 @@ async function bootstrap() {
             persistAuthorization: true,
         },
     });
-    app.useStaticAssets(join(__dirname, '..', 'public'));
-    app.setViewEngine('ejs');
     await app.listen(process.env.PORT);
 }
 bootstrap();
