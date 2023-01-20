065a598c70beb2dff9caeb00dd5fe45b96200510
MamadTvl <mtavallai80@gmail.com>
2023-01-20 17:14:15 +0330

    add eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import babel from 'vite-plugin-babel';
+import eslint from 'vite-plugin-eslint';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), babel()],
+    plugins: [react(), eslint(), babel()],
 });
