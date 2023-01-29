773527c7a688ac7a53d39df2db5bd88f9fe0c0da
MamadTvl <mtavallai80@gmail.com>
2023-01-29 18:57:27 +0330

    reduce bundle size

--- frontend/.babelrc.js
+++ frontend/.babelrc.js
@@ -1,3 +1,24 @@
+const plugins = [
+    [
+        'babel-plugin-import',
+        {
+            libraryName: '@mui/material',
+            libraryDirectory: '',
+            camel2DashComponentName: false,
+        },
+        'core',
+    ],
+    [
+        'babel-plugin-import',
+        {
+            libraryName: '@mui/icons-material',
+            libraryDirectory: '',
+            camel2DashComponentName: false,
+        },
+        'icons',
+    ],
+];
 module.exports = {
     presets: ['@babel/preset-react', '@babel/preset-typescript'],
+    plugins,
 };
